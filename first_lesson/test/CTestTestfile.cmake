5b87b6dc2011f485525f0f2bf2ed93e5c743a153
Talloju, Bharath <Bharath.Talloju@Teradata.com>
2019-01-09 16:18:59 +0530

    removed cmake files

--- first_lesson/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/bt186018/Documents/Private/MRND/SMEC/smec_online_tool/first_lesson/test
-# Build directory: C:/Users/bt186018/Documents/Private/MRND/SMEC/smec_online_tool/first_lesson/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
